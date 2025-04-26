37290e4ba7cee0046a7995aa1ba23992b30a92ce
Ajiwunmi <ajiwunmi@gmail.com>
2025-04-26 22:43:38 +0100

    Ads model updated

--- src/models/Ad.ts
+++ src/models/Ad.ts
@@ -1,22 +1,6 @@
 // models/Ad.ts
 import mongoose, { Schema } from 'mongoose';
 
-const AdSchema = new Schema(
-  {
-    title: String,
-    price: String,
-    category: String,
-    description: String,
-    image: String,
-  },
-  { timestamps: true },
-);
-
-export const Ad = mongoose.models.Ad || mongoose.model('Ad', AdSchema);
-
-
-
-import mongoose from "mongoose";
 
 const AdSchema = new mongoose.Schema(
   {
